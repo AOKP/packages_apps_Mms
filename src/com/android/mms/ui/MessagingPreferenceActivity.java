695347b9844e28bc3fbf02c519fc4f7d0c11c460
qnhoang81 <qnhoang81@yahoo.com>
2012-08-15 03:49:48 -0700

    Clean up Emoji patch from CM9 for the MMS app
    Support for the Unicode subset dedicated for emojis, as used on the iPhone, or even the Gmail website.
    
    Emojis are supported system-wide (display-only) since 4.1 (some glyphs are simply integrated in the system truetype font for the corresponding Unicode code-points).
    These black-and-white emojis are displayed in the notification bar, courtesy of Google, but they don't look good in the Conversation view of the MMS app.
    This patch adds the ability to send emojis back (as in CM9) and replaces them with their colored bitmap version.
    
    For more information, see commit with change-Id: I9eb5081eb7514cb2b89ed3b91cc3054347d6ac90
    
    Change-Id: Iea7dccadf3cb26ef00502631aa0863557b2e8f6e
    
    Conflicts:
    
    	res/values/strings.xml
    	res/xml/preferences.xml
    	src/com/android/mms/ui/ComposeMessageActivity.java
    	src/com/android/mms/ui/MessagingPreferenceActivity.java

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -64,15 +64,6 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String RETRIEVAL_DURING_ROAMING = "pref_key_mms_retrieval_during_roaming";
     public static final String AUTO_DELETE              = "pref_key_auto_delete";
 
-    public static final String MANAGE_TEMPLATES = "pref_key_templates_manage";
-    public static final String SHOW_GESTURE = "pref_key_templates_show_gesture";
-    public static final String GESTURE_SENSITIVITY = "pref_key_templates_gestures_sensitivity";
-    public static final String GESTURE_SENSITIVITY_VALUE = "pref_key_templates_gestures_sensitivity_value";
-    public static final String FULL_TIMESTAMP           = "pref_key_mms_full_timestamp";
-    public static final String SENT_TIMESTAMP           = "pref_key_mms_use_sent_timestamp";
-    public static final String NOTIFICATION_VIBRATE_PATTERN = "pref_key_mms_notification_vibrate_pattern";
-    public static final String NOTIFICATION_VIBRATE_PATTERN_CUSTOM = "pref_key_mms_notification_vibrate_pattern_custom";
-    public static final String NOTIFICATION_VIBRATE_CALL ="pre_key_mms_notification_vibrate_call";
     public static final String ENABLE_EMOJIS = "pref_key_enable_emojis";
     // Menu entries
     private static final int MENU_RESTORE_DEFAULTS    = 1;
