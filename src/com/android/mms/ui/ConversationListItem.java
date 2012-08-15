616d1b37718dd30c5e65aa970a2dad8111a2efbb
qnhoang81 <qnhoang81@yahoo.com>
2012-08-14 19:25:48 -0700

    Emoji patch from CM9 for the MMS app
    Support for the Unicode subset dedicated for emojis, as used on the iPhone, or even the Gmail website.
    
    Emojis are supported system-wide (display-only) since 4.1 (some glyphs are simply integrated in the system truetype font for the corresponding Unicode code-points).
    These black-and-white emojis are displayed in the notification bar, courtesy of Google, but they don't look good in the Conversation view of the MMS app.
    This patch adds the ability to send emojis back (as in CM9) and replaces them with their colored bitmap version.
    
    For more information, see commit with change-Id: I9eb5081eb7514cb2b89ed3b91cc3054347d6ac90
    
    Change-Id: Iea7dccadf3cb26ef00502631aa0863557b2e8f6e
    
    Conflicts:
    
    	res/values/strings.xml
    	res/xml/preferences.xml
    	src/com/android/mms/ui/ComposeMessageActivity.java
    	src/com/android/mms/ui/MessagingPreferenceActivity.java

--- src/com/android/mms/ui/ConversationListItem.java
+++ src/com/android/mms/ui/ConversationListItem.java
@@ -22,13 +22,16 @@ import com.android.mms.R;
 import com.android.mms.data.Contact;
 import com.android.mms.data.ContactList;
 import com.android.mms.data.Conversation;
+import com.android.mms.util.EmojiParser;
 import com.android.mms.util.SmileyParser;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
@@ -211,7 +214,15 @@ public class ConversationListItem extends RelativeLayout implements Contact.Upda
 
         // Subject
         SmileyParser parser = SmileyParser.getInstance();
-        mSubjectView.setText(parser.addSmileySpans(conversation.getSnippet()));
+        CharSequence smileySubject = parser.addSmileySpans(conversation.getSnippet());
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(mContext);
+        boolean enableEmojis = prefs.getBoolean(MessagingPreferenceActivity.ENABLE_EMOJIS, false);
+        if(enableEmojis) {
+            EmojiParser emojiParser = EmojiParser.getInstance();
+            smileySubject = emojiParser.addEmojiSpans(smileySubject);
+        }
+        mSubjectView.setText(smileySubject);
         LayoutParams subjectLayout = (LayoutParams)mSubjectView.getLayoutParams();
         // We have to make the subject left of whatever optional items are shown on the right.
         subjectLayout.addRule(RelativeLayout.LEFT_OF, hasAttachment ? R.id.attachment :
