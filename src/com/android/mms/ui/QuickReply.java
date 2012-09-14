502767e51b0f1a7b1b312214e822d3dc75a39cf9
Adam Fisch <adammfisch@gmail.com>
2012-09-14 15:30:01 -0600

    QuickReply: Quick reply from notification WIP
    This is a WIP! Things are still being worked on!
    Figured you would want to play, and maybe share things
    I have not found broken yet.  Any ideas to make better is also nice :)
    Working:
    -Call from notification
    -Quick Reply from notification
    -Opens its own activity
    -Marks as read
    -Will bypass keyguard and re-open it when reply sent
    -Alerts if you click out of the message box to cancel it or not
    -Clears notification on quick reply
    -Dismiss of quick reply by clicking off screen with options to
    cancel message, stay on message, or mark it read.
    -Emoji & Smiley emote support
    
    TODO:
    -Update Layout (xoomdev is working on one)
    -When getting multiple SMS from one person, show all
    unread from that person in the text body of the popup
    
    -Patch 2: updates to the toast message, and marking SMS read
    -Patch 3: fixed affinity to open activity on its own.
    also now bypasses keyguard and re-opens it on reply.
    -Patch 4: fixed mark as read and clear notification on quick reply
    -Patch 5: update alert with strings to make translators happy
    added the on dismiss to have an option to mark sms read
    -Patch 6: added emoji & smiley support from their built in parsers
    -Patch 7: now marks entire thread as read if you are replying/mark read
    on a text that you have gotten multiple from the same contact
    -Patch 8: update layout: Use android text appearance attr on text,
    use single line on names to avoid wonkiness for very long names,
    add lighter holo send button so that it can be used without a button
    background. Hopefully will work a bit better on < xhdpi devices, no
    way to test here however.
    Change-Id: I012aefbd401d175d8caf834dfb8d7b678cb9a534
    Signed-off-by: Adam Fisch <adammfisch@gmail.com>

---
+++ src/com/android/mms/ui/QuickReply.java
@@ -0,0 +1,296 @@
+
+package com.android.mms.ui;
+
+/*
+ * Copyright (C) 2012 Android Open Kang Project (Adam Fisch & James Roberts)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.android.mms.R;
+import com.android.mms.data.Conversation;
+import com.android.mms.util.EmojiParser;
+import com.android.mms.util.SmileyParser;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.KeyguardManager;
+import android.app.NotificationManager;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.content.DialogInterface.OnDismissListener;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.telephony.SmsManager;
+import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuickReply extends Activity implements OnDismissListener, OnClickListener {
+    Context mContext = this;
+
+    private Bitmap avatar;
+    private Drawable icon;
+    private ImageView contactIcon;
+    private String phoneNumber;
+    private String contactName;
+    private String textBody;
+    private int messageType;
+    private long messageId;
+    private long threadId;
+    private ImageButton sendReply;
+    private TextView nameContact;
+    private TextView prevText;
+    private EditText textBox;
+    private KeyguardManager.KeyguardLock kl;
+    private boolean typing;
+    private boolean wasLocked = false;
+
+    private static final int NOTIFICATION_ID = 123;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        // find if the phone is on the lockscreen to allow dialog popup above it
+        // if needed
+        KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        boolean isLocked = km.inKeyguardRestrictedInputMode();
+
+        kl = km.newKeyguardLock("QuickReply");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        final View mView = inflater.inflate(R.layout.quick_reply_sms, null);
+        AlertDialog alert = new AlertDialog.Builder(this).setView(mView).create();
+
+        Bundle extras = getIntent().getExtras();
+        avatar = (Bitmap) extras.get("avatar");
+        phoneNumber = extras.getString("number");
+        contactName = extras.getString("name");
+        textBody = extras.getString("body");
+        messageId = extras.getLong("msgId");
+        threadId = extras.getLong("threadId");
+        messageType = extras.getInt("count");
+        nameContact = (TextView) mView.findViewById(R.id.contact_name);
+        nameContact.setText(contactName);
+        prevText = (TextView) mView.findViewById(R.id.prev_text_body);
+        prevText.setText(replaceWithEmotes(textBody));
+        contactIcon = (ImageView) mView.findViewById(R.id.contact_avatar);
+        icon = null;
+        icon = getConactAvatar(avatar);
+        if (icon != null) {
+            contactIcon.setBackgroundDrawable(icon);
+        } else {
+            contactIcon.setBackgroundResource(R.drawable.ic_contact_picture);
+        }
+        sendReply = (ImageButton) mView.findViewById(R.id.reply_button);
+        sendReply.setOnClickListener(this);
+        textBox = (EditText) mView.findViewById(R.id.edit_box);
+        textBox.setOnClickListener(this);
+        alert.setOnDismissListener(this);
+        // set alert system to make sure it is always on top, permission
+        // required.
+        alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        if (isLocked) {
+            kl.disableKeyguard();
+            wasLocked = true;
+        }
+        alert.show();
+    }
+
+    private Drawable getConactAvatar(Bitmap bitmap) {
+        Drawable icon;
+        Resources res = mContext.getResources();
+        if (bitmap != null) {
+            final int idealIconHeight =
+                    res.getDimensionPixelSize(android.R.dimen.notification_large_icon_height);
+            final int idealIconWidth =
+                    res.getDimensionPixelSize(android.R.dimen.notification_large_icon_width);
+            if (bitmap.getHeight() < idealIconHeight) {
+                // Scale this image to fit the intended size
+                bitmap = Bitmap.createScaledBitmap(
+                        bitmap, idealIconWidth, idealIconHeight, true);
+            }
+            icon = new BitmapDrawable(getResources(), bitmap);
+        } else {
+            icon = null;
+        }
+        return icon;
+    }
+
+    /**
+     * Use smiley parser and emoji parser to show emotes in text body this
+     * method is modified from the MessageListItem.java
+     * 
+     * @param: body of text from an SMS in String format
+     * @return: formatted text to use smiley emotes & emoji
+     */
+    private CharSequence replaceWithEmotes(String body) {
+        SpannableStringBuilder buf = new SpannableStringBuilder();
+
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(mContext);
+        boolean enableEmojis = prefs.getBoolean(MessagingPreferenceActivity.ENABLE_EMOJIS, false);
+
+        if (!TextUtils.isEmpty(body)) {
+            SmileyParser parser = SmileyParser.getInstance();
+            CharSequence smileyBody = parser.addSmileySpans(body);
+            if (enableEmojis) {
+                EmojiParser emojiParser = EmojiParser.getInstance();
+                smileyBody = emojiParser.addEmojiSpans(smileyBody);
+            }
+            buf.append(smileyBody);
+        }
+        return buf;
+    }
+
+    private void sendSms() {
+        String message = null;
+        message = textBox.getText().toString();
+        SmsManager sms = SmsManager.getDefault();
+        try {
+            sms.sendTextMessage(phoneNumber, null, message, null, null);
+        } catch (IllegalArgumentException e) {
+        }
+        setRead();
+        addMessageToSent(message);
+        finish();
+    }
+
+    /**
+     * mark messages as read single or multi threaded messages and gets the
+     * threadId to use Conversation data to clear the entire thread if the SMS
+     * is multi threaded from a single person
+     */
+    private void setRead() {
+        if (messageType == 1) {
+            Conversation cnv = Conversation.get(mContext, threadId, true);
+            cnv.markAsRead();
+        } else {
+            ContentValues values = new ContentValues();
+            values.put("READ", 1);
+            values.put("SEEN", 1);
+            getContentResolver().update(Uri.parse("content://sms/"),
+                    values, "_id=" + messageId, null);
+        }
+
+        // clear the notification
+        NotificationManager nm = (NotificationManager) mContext.getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        nm.cancel(NOTIFICATION_ID);
+    }
+
+    private void addMessageToSent(String messageSent) {
+        ContentValues sentSms = new ContentValues();
+        sentSms.put("address", phoneNumber);
+        sentSms.put("body", messageSent);
+        getContentResolver().insert(Uri.parse("content://sms/sent"), sentSms);
+    }
+
+    @Override
+    public void onClick(View v) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (v == sendReply) {
+            if (typing) {
+                imm.hideSoftInputFromWindow(textBox.getWindowToken(), 0);
+                typing = false;
+            }
+            sendSms();
+            String nyan = getResources().getString(R.string.quick_reply_sending);
+            Toast.makeText(this, nyan + ": " + contactName, Toast.LENGTH_SHORT).show();
+        } else if (v == textBox) {
+            imm.showSoftInput(textBox, 0);
+            typing = true;
+            textBox.requestFocus();
+        }
+
+    }
+
+    @Override
+    public void onDestroy() {
+        if (wasLocked) {
+            kl.reenableKeyguard();
+        }
+        finish();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle(R.string.qr_alert_title);
+        alert.setMessage(R.string.qr_alert_message);
+
+        alert.setPositiveButton(R.string.qr_alert_yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                finish();
+            }
+        });
+
+        alert.setNegativeButton(R.string.qr_alert_no, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                LayoutInflater inflater = LayoutInflater.from(QuickReply.this);
+                final View mView = inflater.inflate(R.layout.quick_reply_sms, null);
+                AlertDialog alert = new AlertDialog.Builder(QuickReply.this).setView(mView)
+                        .create();
+
+                nameContact = (TextView) mView.findViewById(R.id.contact_name);
+                nameContact.setText(contactName);
+                prevText = (TextView) mView.findViewById(R.id.prev_text_body);
+                prevText.setText(replaceWithEmotes(textBody));
+                contactIcon = (ImageView) mView.findViewById(R.id.contact_avatar);
+                icon = null;
+                icon = getConactAvatar(avatar);
+                if (icon != null) {
+                    contactIcon.setBackgroundDrawable(icon);
+                } else {
+                    contactIcon.setBackgroundResource(R.drawable.ic_contact_picture);
+                }
+                sendReply = (ImageButton) mView.findViewById(R.id.reply_button);
+                sendReply.setImageResource(R.drawable.ic_send_holo_light);
+                sendReply.setOnClickListener(QuickReply.this);
+                textBox = (EditText) mView.findViewById(R.id.edit_box);
+                textBox.setOnClickListener(QuickReply.this);
+                alert.setOnDismissListener(QuickReply.this);
+                alert.show();
+            }
+        });
+
+        alert.setNeutralButton(R.string.qr_alert_read, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                setRead();
+                finish();
+            }
+        });
+
+        alert.show();
+    }
+}
