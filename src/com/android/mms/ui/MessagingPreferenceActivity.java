1d99ee3adc463f995f8812ebb84df409bb89b2be
gzfighter <dev@gzfighter.info>
2012-10-03 20:32:11 -0400

    Mms: added the option to change the soft keyboard type when composing messages
    	This commit gives the user the option to change the keyboard from Emoji
    to enter for a new line or enter to send.
    The setting has no effect on the hardware keyboard.
    
    Change-Id: I9bd661dac747652ad01e3b002aa294f691b87a37

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -67,6 +67,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String DISPLAY_QR_CALLBUTTON    = "pref_key_display_quickreply_callbutton";
 
     public static final String ENABLE_EMOJIS = "pref_key_enable_emojis";
+    public static final String INPUT_TYPE    = "pref_key_mms_input_type";
     // Menu entries
     private static final int MENU_RESTORE_DEFAULTS    = 1;
 
