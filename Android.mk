b3fef86941321e67392d7ad87e4f45879fe98461
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-03-09 11:47:16 +0100

    Mms: 'Open' in QR menu & resume sleep after sending msg from lockscreen
    This adds an 'open' to the qr menu which allows you to enter
    the current conversation(in turn clearing notification and
    marking read/seen)
    
    Second feature is optional and allows the device to be turned
    back off when launching qr from lockscreen after its been relocked.
    (device off->on->check notifs->use qr->relock->screen off)
    
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    Change-Id: Idd35e66f514ccf626995d2be5fc6af44500c5ba4

--- Android.mk
+++ Android.mk
@@ -13,6 +13,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := Mms
+LOCAL_CERTIFICATE := platform
 
 # Builds against the public SDK
 #LOCAL_SDK_VERSION := current
