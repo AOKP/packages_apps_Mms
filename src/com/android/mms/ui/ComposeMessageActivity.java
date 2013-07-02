f294ca63055a44441259684020c1263deb7f47b1
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-07-02 07:00:05 +0200

    Mms: Allow contact via SMS on number longpress in conversation
    * Allow contacting number via SMS when longpressing number within an SMS/MMS conversation.
      Single touch will enter this number into dialer already.
    
    PS2: Combine if statements into uno
    
    Change-Id: I946048bfd0570222d0fa44987dba793acd0645e8
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -247,6 +247,8 @@ public class ComposeMessageActivity extends Activity
     private static final int LOAD_TEMPLATE_BY_ID        = 0;
     private static final int LOAD_TEMPLATES             = 1;
 
+    private static final int MENU_SMS_NUMBER_SELECTED   = 34;
+
     private static final int RECIPIENTS_MAX_LENGTH = 312;
 
     private static final int MESSAGE_LIST_QUERY_TOKEN = 9527;
@@ -1061,6 +1063,16 @@ public class ComposeMessageActivity extends Activity
                 menu.add(0, MENU_ADD_ADDRESS_TO_CONTACTS, 0, addContactString)
                     .setOnMenuItemClickListener(l)
                     .setIntent(intent);
+
+                Intent sms = createIntent(this, 0);
+                sms.setData(Uri.parse("sms:" + uriString));
+
+                String addSmsString = getString(R.string.menu_sms_number_selected,
+                    uriString);
+                menu.add(0, MENU_SMS_NUMBER_SELECTED, 0, addSmsString)
+                    .setOnMenuItemClickListener(l)
+                    .setIntent(sms);
+                saveDraft(false);
             }
         }
     }
