18b4ac8c083ef4171ae3d313c8faa1da1afcf4f1
Steve Kondik <shade@chemlab.org>
2013-10-13 23:02:53 -0400

    Mms: Add resend function for message
    PS2: Fixed whitespaces and czech translation
    PS2: Deleted rest of whitespaces
    
    Change-Id: Ifb25d3d55a6f972cffcb2c282c20554b4bacf226
    
    Conflicts:
    	res/values-cs/strings.xml
    	src/com/android/mms/data/WorkingMessage.java
    	src/com/android/mms/ui/ComposeMessageActivity.java

--- src/com/android/mms/data/WorkingMessage.java
+++ src/com/android/mms/data/WorkingMessage.java
@@ -159,6 +159,9 @@ public class WorkingMessage {
 
     private static final int MMS_MESSAGE_SIZE_INDEX  = 1;
 
+    // flag indicate resend sms that the recipient of conversion is more than one.
+    private boolean mResendMultiRecipients;
+
     /**
      * Callback interface for communicating important state changes back to
      * ComposeMessageActivity.
@@ -1318,6 +1321,20 @@ public class WorkingMessage {
             // Be paranoid and clean any draft SMS up.
             deleteDraftSmsMessage(threadId);
         }
+
+        // just do a regular send. We're already on a non-ui thread so no need to fire
+        // off another thread to do this work.
+        if (mResendMultiRecipients) {
+            Log.d(TAG, "it is resend sms recipient="+recipientsInUI);
+            sendSmsWorker(msgText, recipientsInUI, threadId);
+            mResendMultiRecipients = false;
+        } else {
+            sendSmsWorker(msgText, semiSepRecipients, threadId);
+        }
+
+        // Be paranoid and clean any draft SMS up.
+        deleteDraftSmsMessage(threadId);
+
     }
 
     private void sendSmsWorker(String msgText, String semiSepRecipients, long threadId) {
@@ -1823,6 +1840,14 @@ public class WorkingMessage {
         // to clear those messages as well as ones with a valid thread id.
         final String where = Mms.THREAD_ID +  (threadId > 0 ? " = " + threadId : " IS NULL");
         asyncDelete(Mms.Draft.CONTENT_URI, where, null);
+    }
+
+    public void setResendMultiRecipients(boolean bResendMultiRecipients) {
+        mResendMultiRecipients = bResendMultiRecipients;
+    }
+
+    public boolean getResendMultiRecipients() {
+        return mResendMultiRecipients;
     }
 
     /**
