812b88c81cc71585a10e3958016da264a87a90f6
Arnav Gupta <championswimmer@aokp.co>
2014-01-11 10:38:21 +0100

    Revert "Mms: Add resend function for message"
    This reverts commit d24f926bdc53ad751368693e7f6d58730ac6c297.
    
    Change-Id: I992afc2167486a6c56ac2331a51bb0bdd5bdf8cc
    
    Conflicts:
    	res/values-zh-rCN/strings_custom.xml
    	res/values/strings_custom.xml
    	src/com/android/mms/ui/ComposeMessageActivity.java

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -247,8 +247,6 @@ public class ComposeMessageActivity extends Activity
     private static final int LOAD_TEMPLATE_BY_ID        = 0;
     private static final int LOAD_TEMPLATES             = 1;
 
-    private static final int MENU_RESEND = 34;
-
     private static final int RECIPIENTS_MAX_LENGTH = 312;
 
     private static final int MESSAGE_LIST_QUERY_TOKEN = 9527;
@@ -364,9 +362,6 @@ public class ComposeMessageActivity extends Activity
 
     // Add SMS to calendar reminder
     private static final String CALENDAR_EVENT_TYPE = "vnd.android.cursor.item/event";
-
-    // record the resend sms recipient when the sms send to more than one recipient
-    private String mResendSmsRecipient;
 
     /**
      * Whether this activity is currently running (i.e. not paused)
@@ -1150,12 +1145,6 @@ public class ComposeMessageActivity extends Activity
                         .setOnMenuItemClickListener(l);
             }
 
-            // only failed send message have resend function
-            if (msgItem.isFailedMessage()) {
-                menu.add(0, MENU_RESEND, 0, R.string.menu_resend)
-                        .setOnMenuItemClickListener(l);
-            }
-
             if (msgItem.isMms()) {
                 switch (msgItem.mBoxId) {
                     case Mms.MESSAGE_BOX_INBOX:
@@ -1374,32 +1363,6 @@ public class ComposeMessageActivity extends Activity
         }, R.string.building_slideshow_title);
     }
 
-    private void resendMessage(MessageItem msgItem) {
-        if (msgItem.isMms()) {
-            // if it is mms, we delete current mms and use current mms
-            // uri to create new working message object.
-            WorkingMessage newWorkingMessage = WorkingMessage.load(this, msgItem.mMessageUri);
-            if (newWorkingMessage == null)
-                return;
-
-            // Discard the current message in progress.
-            mWorkingMessage.discard();
-            mWorkingMessage = newWorkingMessage;
-            mWorkingMessage.setConversation(mConversation);
-            mWorkingMessage.setSubject(msgItem.mSubject, false);
-        } else {
-            if (getRecipients().size() > 1) {
-                // if the number is more than one when send sms, there will show serveral msg items
-                // the recipient of msg item is not equal with recipients of conversation
-                // so we should record the recipient of this msg item.
-                mWorkingMessage.setResendMultiRecipients(true);
-                mResendSmsRecipient = msgItem.mAddress;
-            }
-            editSmsMessageItem(msgItem);
-        }
-        sendMessage(true);
-    }
-
     /**
      * Context menu handlers for the message list view.
      */
@@ -1428,10 +1391,6 @@ public class ComposeMessageActivity extends Activity
 
                 case MENU_FORWARD_MESSAGE:
                     forwardMessage(mMsgItem);
-                    return true;
-
-                case MENU_RESEND:
-                    resendMessage(mMsgItem);
                     return true;
 
                 case MENU_VIEW_SLIDESHOW:
@@ -3971,6 +3930,7 @@ public class ComposeMessageActivity extends Activity
 
             // strip unicode chars before sending (if applicable)
             mWorkingMessage.setText(stripUnicodeIfRequested(mWorkingMessage.getText()));
+
             mWorkingMessage.send(mDebugRecipients);
 
             mSentMessage = true;
