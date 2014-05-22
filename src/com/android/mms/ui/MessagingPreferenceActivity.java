omnirom/android_packages_apps_Mms

[2/4] Mms: Smart Phone Call
Put on your ear, and this will automatically call current number in message or dialer

Change-Id: Id53ab341c4eb24b6757f418a4516a4f3e87bc7f1

src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -39,6 +39,7 @@ import android.preference.PreferenceScreen;
 import android.preference.RingtonePreference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.provider.SearchRecentSuggestions;
+import android.provider.Settings;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -691,6 +692,13 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         boolean qmDarkThemeEnabled =
             prefs.getBoolean(MessagingPreferenceActivity.QM_DARK_THEME_ENABLED, false);
         return qmDarkThemeEnabled;
+    }
+
+    public static boolean getSmartCallEnabled(Context context) {
+        int enabled = Settings.System.getInt(context.getContentResolver(),
+                          Settings.System.SMART_PHONE_CALLER, 0);
+        boolean smartCallEnabled = (enabled != 0);
+        return smartCallEnabled;
     }
 
     private void registerListeners() {

AOKP/packages_apps_Mms

src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -40,6 +40,7 @@ import android.preference.PreferenceScreen;
 import android.preference.RingtonePreference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.provider.SearchRecentSuggestions;
+import android.provider.Settings;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -687,6 +688,13 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         boolean qmDarkThemeEnabled =
             prefs.getBoolean(MessagingPreferenceActivity.QM_DARK_THEME_ENABLED, false);
         return qmDarkThemeEnabled;
+    }
+
+    public static boolean getSmartCallEnabled(Context context) {
+        int enabled = Settings.AOKP.getInt(context.getContentResolver(),
+                          Settings.AOKP.SMART_PHONE_CALLER, 0);
+        boolean smartCallEnabled = (enabled != 0);
+        return smartCallEnabled;
     }
 
     private void registerListeners() {